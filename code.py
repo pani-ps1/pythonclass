fb9de55f216c394f183ec1371e024e93c83ebf3b
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-05-27 07:34:09 -0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,14 @@
+print("yek adad vared kon")
+first_number = input()
+first_number_int = int(first_number)
+
+
+print("bazam adad vared kon")
+second_number = input()
+second_number_int = int(second_number)
+
+
+
+result = first_number_int + second_number_int 
+
+print(result)
