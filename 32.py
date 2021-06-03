6a1a4c663233782775406a0128ea631fef055157
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-03 07:20:00 -0300

    Add files via upload

---
+++ 32.py
@@ -0,0 +1,40 @@
+#def bmi():
+    #a = ghad
+    #b = vazn
+    #a = float(input("vazn khod ra be kilg vared konid:\n"))
+    #b = float(input("ghad khod ra be mtr vared konid:\n"))
+    #bmi = vazn / (ghad * ghad)
+    #print("bmi ")
+
+
+#def calculate_bmi():
+    #vazn = int(input("vazn khod ra vared konid\n"))
+    #ghad = float(input("ghad khod ra vared konid\n"))
+
+    #bmi = vazn /(ghad** 2)
+    #print(f"bmi shom {bmi} ast")
+
+#calculat_bmi()
+
+
+
+#def call_ali():
+    #name = "ali"
+    #if name =="ali":
+    #    print("salam ali")
+#print (name)
+
+#name = "sanaz"
+#def print_name():
+#    name = "saeed"
+#    print("salam", name)
+
+#print_name()
+
+def adad_nahai():
+    adad = float(input("adad ra benevisid:\n"))
+
+    adad_nahai = adad * 2
+    print(adad_nahai)
+
+adad_nahai()
