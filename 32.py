e971ccc31cd41b7d885ad42a7013d2dd62bfb0fb
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-03 08:20:38 -0300

    Add files via upload
    1

--- 32.py
+++ 32.py
@@ -24,6 +24,8 @@
     #    print("salam ali")
 #print (name)
 
+
+
 #name = "sanaz"
 #def print_name():
 #    name = "saeed"
@@ -31,10 +33,59 @@
 
 #print_name()
 
-def adad_nahai():
-    adad = float(input("adad ra benevisid:\n"))
 
-    adad_nahai = adad * 2
-    print(adad_nahai)
 
-adad_nahai()
+#def adad_nahai():
+#    adad = float(input("adad ra benevisid:\n"))
+
+#    adad_nahai = adad * 2
+#    print(adad_nahai)
+
+#adad_nahai()
+
+#def begu_salam(name):
+#    print(f"salam {name}")
+
+#name = input("ye esm begu")
+#defbegu_salam()
+
+
+#adad =
+#def sen_shakhs(adad):
+#    adad = int(input("sal tavalod khod ra vared konid:\n"))
+    #sen_shakhs = 1400 - int (adad)
+
+#sen_shakhs(adad)
+
+#def sen_shaks(adad):
+#    print(f"sen shoma {adad}")
+
+#sen_shaks = 1400 - inti(input(adad))
+#sen_shaks(adad)
+
+
+#def sen_shaks(sal_tavalod,sen)
+#    print("sen shoma",)
+
+#def calculate_age(sale_tavalod):
+#    age = 1400 - sale_tavalod
+#    print("age:",age)
+
+#input_age = int(input("enter your birth year"))
+#calculate_age(input_age)
+
+
+
+#def masahat_mostatil(tool,arz):
+#    masahat = tool * arz
+#    print("masahat in mostatil = ", masahat)
+
+##input_masahat = float(input())
+#masahat_mostatil(12,3)
+##age yekish nabashe mige ye argoman kame
+
+def mohit_mostatil(tool,arz):
+    mohit = (tool +arz )* 2
+    print("mohit dar mostatil = ",mohit)
+
+mohit_mostatil(12,3)
