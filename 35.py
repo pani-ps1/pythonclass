70b742b523500070bec603d69dffe3feaf418f13
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-03 09:48:29 -0300

    Add files via upload

---
+++ 35.py
@@ -0,0 +1,15 @@
+#def calculate_age(brith_year):
+#    age = 1400 - brith_year
+#    return age
+
+#def who_is_ali(age):
+#    print(f"ali kasi ast ke {age}sal sen darad")
+
+#age = calculat_bmi(1312)
+
+
+def adad_gereftan(adad):
+    x = adad + 1
+    return x
+
+adad_gereftan(2)
