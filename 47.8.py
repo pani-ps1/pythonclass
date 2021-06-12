ceace666e35932a4a77c38140348ad4219912ea9
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-12 02:52:15 -0300

    Add files via upload

---
+++ 47.8.py
@@ -0,0 +1,9 @@
+start = 0
+
+while start <= 100:
+    print(start)
+    command = input("bazam mikhay beram bala?\n")
+    if command == "are":
+        start += 1
+    elif command == "na"
+        break
