9591088c53e0f05585e97f4520362769defddcac
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-12 02:51:31 -0300

    Add files via upload

---
+++ 47.11.py
@@ -0,0 +1,16 @@
+start = 0
+
+while start < 42:
+    start += 1
+    if start % 2 == 0:
+    else:
+        print(start)
+
+        continue
+
+
+while True:
+    name = input("esme to begu:\n")
+    if name =="paniz":
+        continue
+    print(f"salam {name}")
