ae7098fa3bf7f486ae45597fdce42a8cae80bbb2
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-13 06:44:59 -0300

    Add files via upload

---
+++ 47.19.tamrin3_bakhsh_paziri.py
@@ -0,0 +1,11 @@
+#adad 1 _ 10
+
+i = 0
+j = int(input("adad khod ra vared konid:\n"))
+
+while i < 100:
+    i += 1
+    if i % j == 0:
+        print("adad pakhsh pazir ast.")
+    else:
+        continue
