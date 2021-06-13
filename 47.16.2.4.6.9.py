0efebd40e8336c355dc81366558ffe0e823d58af
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-13 03:14:26 -0300

    Add files via upload

---
+++ 47.16.2.4.6.9.py
@@ -0,0 +1,26 @@
+#1400/3/23
+#adad 2 , 4 , 6 , 9
+
+start = 0
+
+while start < 100:
+    start += 1
+    if start % 2 == 0:
+        print("in adad bahal bar 2 bakhsh pazir ast.!!")
+    else:
+        continue
+
+    if start % 4 == 0:
+        print("in adad bahal bar 4 bakhsh pazir ast.!!!!")
+    else:
+        continue
+
+    if start % 6 == 0:
+        print("in adad bahal bar 6 bakhsh pazir ast.!!!!!!")
+    else:
+        continue
+
+    if start % 9 == 0:
+        print("in adad bahal bar 9 bakhsh pazir ast.!!!!!!!!!")
+    else:
+        continue
