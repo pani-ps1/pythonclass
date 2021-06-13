cd46b8cc0132550f529be9f2b34375fe1f083ef4
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-13 07:16:45 -0300

    Add files via upload

---
+++ 47.20.tamrin4_bazi_hop.py
@@ -0,0 +1,16 @@
+# tamrin 3
+# bazi hop
+
+i = 0
+j = int(input("ye adad begu:\n"))
+while i < 100:
+    i += 1
+    if i % 5 == 0:
+        print("hop")
+    if i % 5 == 0:
+        continue
+    if j > i:
+        print("tartib ra raayet konid\n")
+    else:
+        print(i)
+    continue
