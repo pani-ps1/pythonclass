04c51d7a97f9e12e3d859de22359c0f11bd5d4d9
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-17 08:14:18 -0400

    Add files via upload

---
+++ 49.7.py
@@ -0,0 +1,5 @@
+gender = ["khanoom", "khanoom", "khanoom", "khanoom", "agha", "agha", "agha", "agha"]
+my_class = ["paniz", "zeinab", "mahnaz", "mehrana","mohamad", "amin","farokh", "reza"]
+
+for gender, my_class in zip(gender, my_class):
+    print(f"salam {gender} {my_class}")
