f151f993ee04c39d4b12e788693fc481fa72edd6
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-17 07:26:53 -0400

    Add files via upload

---
+++ 49.py
@@ -0,0 +1,7 @@
+my_class = "paniz zeinab mahnaz mehrana mohamad amin farokh reza meysam".split(" ")
+
+
+for name in range("my_class"):
+    for j in renge(adad):
+        adad += 1
+print(f"{name} nafar {adad} ast.\n")
