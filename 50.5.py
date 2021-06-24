9a1a8bc370b8000b11bcad867f7aebb770caf269
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-24 06:24:55 -0400

    Add files via upload

---
+++ 50.5.py
@@ -0,0 +1,46 @@
+my_class = ["paniz", "zeinab", "mahnaz", "mehrana", "mohamad", "amin"]
+last_name =["sedaghat", "zebihi", "ahmadi", "gerayeli", "shahsavari",
+ "noruzi"]
+
+for i in my_class:
+    for j in last_name:
+        print(i,"\t"+ j )
+
+
+#-----------------------------
+#for i in range(len(my_class)):
+    for j in range(len(last_name)):
+        result = my_class + last_name
+#print(result)
+
+#------------------------------
+#result = ["","","","","",""]
+
+#for i in range(my_class):
+#    for j in range(last_name):
+#        result = my_class + last_name
+
+#for r in result:
+#    print(r)
+
+#------------------------
+
+#matrix = [["paniz", "zeinab", "mahnaz", "mehrana","mohamad", "amin"],["\t"],
+# ["sedaghat", "zebihi", "ahmadi", "gerayeli", "shahsavari","noruzi",]]
+
+
+#for row in matrix:
+#    for item in row:
+#        print(item)
+
+#-------------------------
+#for index, name in enumerate(my_class):
+#    for index, name2 in enumerate(last_name):
+#        print(f"salam {my_class} {last_name}")
+
+#--------------------------------
+#for name in my_class:
+#    if name.index(1) > 0
+#        name.index(1 + 1)
+#    for name2 in last_name:
+#        print(name + name2)
