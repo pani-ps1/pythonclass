76c3bc52f28f2cd04a78ba52920a50e578e40c6e
pani-ps1 <70625901+pani-ps1@users.noreply.github.com>
2021-06-25 03:06:52 -0400

    Add files via upload

---
+++ 51.7.tamrin1.jalase6.py
@@ -0,0 +1,6 @@
+my_class = ["paniz", "zeinab", "mahnaz", "mehrana", "mohamad", "amin", "paniz", "paniz"]
+#my_class.fromkeys(paniz,2)
+
+new_class = list(dict.fromkeys(my_class))
+
+print(new_class)
